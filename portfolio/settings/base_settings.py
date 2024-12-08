a51d9d1a1c5267dde8b47f2b817c284a411dd2f2
jlee1028 <jasonlee1028@yahoo.com>
2024-12-07 16:33:24 -0800

    separate dev and prod settings

--- portfolio/settings/base_settings.py
+++ portfolio/settings/base_settings.py
@@ -12,22 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from utils.secret_manager import get_secret
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret('django-secret-key', 'us-west-2')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['.jasonsworkspace.com']
 
 
 # Application definition
@@ -75,17 +62,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "portfolio.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -121,33 +97,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = "/var/www/jasonsworkspace.com/static"
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-# Require HTTPS
-SECURE_HSTS_SECONDS = 2_592_000  # 30 days
-SECURE_HSTS_PRELOAD = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
-# Set referrer policy
-SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
-
-# Add content security policy
-MIDDLEWARE += ["csp.middleware.CSPMiddleware"]
-
-# Allow browser to load external links
-# ! replace 'unsafe-inline' with nonce
-CSP_STYLE_SRC = ["'self'", "cdn.jsdelivr.net", "code.jquery.com", "stackpath.bootstrapcdn.com", "'unsafe-inline'"]
-
-# Allow browser to load external scripts
-CSP_SCRIPT_SRC = ["'self'", "cdn.jsdelivr.net", "code.jquery.com"]
-
-# Allow browser to load external images
-CSP_IMG_SRC = ["'self'", "data:"]
-
