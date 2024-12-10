bdeeccf8b0767aad452f510f0fa21e240d76c1e8
jlee1028 <jasonlee1028@yahoo.com>
2024-12-09 20:49:59 -0800

    update django development settings

--- portfolio/settings/development.py
+++ portfolio/settings/development.py
@@ -3,11 +3,11 @@ from .base_settings import *
 
 SECRET_KEY = 'abc123'
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.jasonsworkspace.dev', 'localhost', '127.0.0.1']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'jws_db',
+        'NAME': 'jws_db_dev',
         'USER': 'jason',
         'PASSWORD': 'password',
         'HOST': 'localhost',
